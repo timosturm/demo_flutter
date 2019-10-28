3cc0d5ee51a492c4ca74d7b6dec85ae9bd49ab01
Timo Sturm <timo.sturm@netknights.it>
2019-10-28 11:22:21 +0100

    added localization stuff

--- startup_namer/lib/customizationDemo.dart
+++ startup_namer/lib/customizationDemo.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'appTheme.dart' as appTheme;
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+
+import 'l10n/messages_all.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Customization Demo',
+      localizationsDelegates: [
+        const DemoLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('de', ''),
+      ],
+      onGenerateTitle: (BuildContext c) =>
+      DemoLocalizations
+          .of(c)
+          .title,
       theme: appTheme.getTheme(),
       home: RandomWords(),
     );
@@ -16,56 +31,16 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-
 //  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: 20,
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-//          if (index >= _suggestions.length) {
-//            _suggestions.addAll(generateWordPairs().take(10));
-//          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-//    final bool alreadySaved = _saved.contains(pair);
-    final bool alreadySaved = true;
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-//            _saved.remove(pair);
-          } else {
-//            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("I just dont know!"),
+        title: Text(DemoLocalizations
+            .of(context)
+            .title),
         leading: Padding(
           padding: EdgeInsets.all(4.0),
           child: Image.asset('assets/logo/logo.png'),
@@ -78,47 +53,135 @@ class RandomWordsState extends State<RandomWords> {
           ),
         ],
       ),
-      body: _buildSuggestions(),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Text(DemoLocalizations
+                  .of(context)
+                  .greeting),
+              Divider(),
+              Text(DemoLocalizations
+                  .of(context)
+                  .introductionWithName),
+              Divider(),
+              Text(DemoLocalizations
+                  .of(context)
+                  .introductionWithAge),
+              Divider(),
+              Text(DemoLocalizations
+                  .of(context)
+                  .questionHowAreYou),
+              Divider(),
+              Text(DemoLocalizations
+                  .of(context)
+                  .questionCanIHelp),
+            ],
+          ),
+      ),
     );
   }
 
   Future<String> loadAsset(BuildContext context, key) async {
     return DefaultAssetBundle.of(context).loadString(key);
   }
-
-//  void _pushSaved() {
-//    Navigator.of(context).push(
-//      MaterialPageRoute<void>(
-//        builder: (BuildContext context) {
-//          final Iterable<ListTile> tiles = _saved.map(
-//            (WordPair pair) {
-//              return ListTile(
-//                title: Text(
-//                  pair.asPascalCase,
-//                  style: _biggerFont,
-//                ),
-//              );
-//            },
-//          );
-//
-//          final List<Widget> divided = ListTile.divideTiles(
-//            context: context,
-//            tiles: tiles,
-//          ).toList();
-//
-//          return Scaffold(
-//            appBar: AppBar(
-//              title: Text('Saved Suggestions'),
-//            ),
-//            body: ListView(children: divided),
-//          );
-//        },
-//      ),
-//    );
-//  }
 }
 
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
+}
+
+class DemoLocalizations {
+  String localeName;
+
+  DemoLocalizations(this.localeName);
+
+  static Future<DemoLocalizations> load(Locale locale) {
+    final String name =
+    locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
+    final String localeName = Intl.canonicalizedLocale(name);
+
+    return initializeMessages(localeName).then((_) {
+      return DemoLocalizations(localeName);
+    });
+  }
+
+  static DemoLocalizations of(BuildContext context) {
+    return Localizations.of<DemoLocalizations>(context, DemoLocalizations);
+  }
+
+  String get title {
+    return Intl.message(
+      'Hello World',
+      name: 'title',
+      desc: 'Title for the Demo application',
+      locale: localeName,
+    );
+  }
+
+  String get greeting {
+    return Intl.message(
+      'Good Day.',
+      name: 'greeting',
+      desc: 'Greeting the user of the app',
+      locale: localeName,
+    );
+  }
+
+  String get introductionWithName {
+    return Intl.message(
+      'Good Day.',
+      name: 'greeting',
+      desc: 'Greeting the user of the app',
+      locale: localeName,
+    );
+  }
+
+  String get introductionWithAge {
+    return Intl.message(
+      'Good Day.',
+      name: 'greeting',
+      desc: 'Greeting the user of the app',
+      locale: localeName,
+    );
+  }
+
+  String get questionHowAreYou {
+    return Intl.message(
+      'Good Day.',
+      name: 'greeting',
+      desc: 'Greeting the user of the app',
+      locale: localeName,
+    );
+  }
+
+  String get questionCanIHelp {
+    return Intl.message(
+      'Good Day.',
+      name: 'greeting',
+      desc: 'Greeting the user of the app',
+      locale: localeName,
+    );
+  }
+}
+
+class DemoLocalizationsDelegate
+    extends LocalizationsDelegate<DemoLocalizations> {
+  const DemoLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'de'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<DemoLocalizations> load(Locale locale) {
+    return DemoLocalizations.load(locale);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<DemoLocalizations> old) {
+    return false;
+  }
 }
