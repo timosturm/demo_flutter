128b8636767c1195d14083c31781f3c498016c82
Timo Sturm <timo.sturm@netknights.it>
2019-11-01 13:43:01 +0100

    changed layout and added dependency

--- startup_namer/lib/secureStorageDemo.dart
+++ startup_namer/lib/secureStorageDemo.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_keychain/flutter_keychain.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,57 +44,89 @@ class MyCustomFormState extends State<MyCustomForm> {
 
 //  final storage = FlutterSecureStorage();
   final key = "key";
+  List<String> valuesInput = List();
+  List<String> valuesStored = List();
 
   final _valueController = TextEditingController();
 
-  _saveValue(value) {
+  _saveValues(value) {
     // TODO check plugin implementation in terms of security
-    FlutterKeychain.put(key: key, value: value);
+//    FlutterKeychain.put(key: key, value: value);
   }
 
-  _showValue() {
+  _showValues() {
     // TODO check plugin implementation in terms of security
-    FlutterKeychain.get(key: key).then(print);
+//    FlutterKeychain.get(key: key).then(print);
   }
 
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: _valueController,
-            decoration:
-                new InputDecoration(hintText: "Value to save securely."),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _valueController,
+                    decoration: new InputDecoration(
+                        hintText: "Value to save securely."),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: RaisedButton(
+                      onPressed: () {
+// Validate returns true if the form is valid, or false
+// otherwise.
+                        if (_formKey.currentState.validate()) {
+// If the form is valid, display a Snackbar.
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Processing Data')));
+                          String text = _valueController.text;
+                          _valueController.clear();
+                          _saveValues(text);
+                          _showValues();
+                          setState(() {
+                            valuesInput.add(text);
+                          });
+                        }
+                      },
+                      child: Text('Save Value'),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: RaisedButton(
+            onPressed: () {
+              // TODO
             },
+            child: Text('Show all Values'),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a Snackbar.
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Processing Data')));
-                  _saveValue(_valueController.text);
-                  _showValue();
-                }
-              },
-              child: Text('Save'),
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: RaisedButton(
+            onPressed: () {
+              // TODO
+            },
+            child: Text('Delete All Values'),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
