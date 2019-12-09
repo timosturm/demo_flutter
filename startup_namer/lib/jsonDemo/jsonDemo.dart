1d77ea2f069f5a8d845e4397ee879128865aa9d3
Timo Sturm <timo.sturm@netknights.it>
2019-12-09 12:14:11 +0100

    added serialization and deserialtization

--- startup_namer/lib/jsonDemo/jsonDemo.dart
+++ startup_namer/lib/jsonDemo/jsonDemo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:startup_namer/jsonDemo/Order.dart';
@@ -16,9 +18,34 @@ class JsonTestWidgetState extends State<JsonTestWidget> {
     ChristmasOrder("christAdd", 11, "Hohoho!"),
   ];
 
-  List<Order> serializedOrders = orders;
+  List<Order> serializedOrders = [];
 
-  JsonTestWidgetState() {}
+  JsonTestWidgetState() {
+    print("Liste: $orders");
+
+    List<String> jsons = [];
+    // [{"price":55.0,"address":"orderAdd"}, {"price":33.0,"address":"discOrd","discount":5}, {"price":11.0,"address":"christAdd","greeting":"Hohoho!"}]
+    for (Order order in orders) {
+      jsons.add(jsonEncode(order));
+    }
+
+    print("Strings: $jsons");
+
+    for (String jsString in jsons) {
+      Map orderMap = jsonDecode(jsString);
+
+      Order order = null;
+      if (orderMap.containsKey("discount")) {
+        order = DiscountOrder.fromJson(orderMap);
+      } else if (orderMap.containsKey("greeting")) {
+        order = ChristmasOrder.fromJson(orderMap);
+      } else {
+        order = Order.fromJson(orderMap);
+      }
+
+      serializedOrders.add(order);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
