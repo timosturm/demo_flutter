8cc71017a027b7628cd6a8ba59f32b2a057e1b38
Timo Sturm <timo.sturm@netknights.it>
2019-12-09 12:06:07 +0100

    added annotations and fixed constructors

--- startup_namer/lib/jsonDemo/Order.dart
+++ startup_namer/lib/jsonDemo/Order.dart
@@ -1,3 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Order.g.dart';
+
+//part 'DiscountOrder.g.dart';
+//part 'ChristmasOrder.g.dart';
+
+@JsonSerializable()
 class Order {
   String _address;
   double _price;
@@ -6,23 +14,43 @@ class Order {
 
   String get address => _address;
 
-  Order(this._address, this._price);
+  Order(String address, double price)
+      : this._price = price,
+        this._address = address;
+
+  factory Order.fromJson(Map<String, dynamic> json) => _$OrderFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OrderToJson(this);
 }
 
+@JsonSerializable()
 class DiscountOrder extends Order {
   int _discount;
 
   int get discount => _discount;
 
-  DiscountOrder(String address, double price, this._discount)
-      : super(address, price);
+  DiscountOrder(String address, double price, int discount)
+      : this._discount = discount,
+        super(address, price);
+
+  factory DiscountOrder.fromJson(Map<String, dynamic> json) =>
+      _$DiscountOrderFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DiscountOrderToJson(this);
 }
 
+@JsonSerializable()
 class ChristmasOrder extends Order {
   String _greeting;
 
   String get greeting => _greeting;
 
-  ChristmasOrder(String address, double price, this._greeting)
-      : super(address, price);
+  ChristmasOrder(String address, double price, String greeting)
+      : this._greeting = greeting,
+        super(address, price);
+
+  factory ChristmasOrder.fromJson(Map<String, dynamic> json) =>
+      _$ChristmasOrderFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ChristmasOrderToJson(this);
 }
